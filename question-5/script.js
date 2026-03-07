f668d1b1266bf23eb3c06f2cf37af11c5eab2aee
Rimjhim Sharma <rimjhimsharma532@gmail.com>
2026-03-07 21:06:15 +0200

    ques-5.html

---
+++ question-5/script.js
@@ -0,0 +1,18 @@
+//const object called "Product" to store information 
+const Product = {
+    name: "Ball Pen",
+    price: 270,
+    offer: 5,
+    rating: 7.002,
+
+};
+console.log(Product);
+console.log(Product.name);
+console.log(Product.price);
+console.log(Product["offer"]);
+console.log(Product["rating"]);
+//type of operator
+console.log(typeof Product.name);
+console.log(typeof Product.price);
+console.log(typeof Product.offer);
+console.log(typeof Product.rating);
