b6f3ae13b6255b7c105a5ff03f1f5313a9b40131
Rimjhim Sharma <rimjhimsharma532@gmail.com>
2026-03-07 23:55:30 +0200

    ques-20.html

---
+++ question-20/script.js
@@ -0,0 +1,12 @@
+//conditional statements
+let age = 16;
+age>=18? "adult" : "not adult";
+console.log(age>=18? "adult" : "not adult");
+ //use of str,charAt()
+ let str = "IloveJS";
+ console.log(str.charAt(2));// skip the two numbes I and L
+  //Temeplate literals
+  console.log("Rimjhim\nSharma");// uper niche likhne  liye rimjhm uper sharma niche
+   
+  let n = "Rimjhim\tSharma";//tab spac
+    console.log(n.length);
