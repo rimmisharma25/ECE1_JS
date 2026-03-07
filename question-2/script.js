27af089f3f2f47702826dabf4e07f5d53e97d146
Rimjhim Sharma <rimjhimsharma532@gmail.com>
2026-03-07 19:42:32 +0200

    ques-2.html

---
+++ question-2/script.js
@@ -0,0 +1,17 @@
+//Artimetic Operators
+let a = 10;
+let b = 5;
+let c;
+c = a + b; // Addition
+console.log("a = ", a, " & b = ", b);
+console.log("Addition: " + c);
+c = a - b; // Subtraction
+console.log("Subtraction: " + c);
+c = a * b; // Multiplication
+console.log("Multiplication: " + c);
+c = a / b; // Division
+console.log("Division: " + c);
+c = a % b; // Modulus
+console.log("Modulus: " + c);
+c = a ** b; // Exponentiation
+console.log("Exponentiation: " + c);
