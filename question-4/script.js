2631de08d80d4b92ae1e311ee29fd2cbce467780
Rimjhim Sharma <rimjhimsharma532@gmail.com>
2026-03-07 20:19:10 +0200

    ques-4.html

---
+++ question-4/script.js
@@ -0,0 +1,19 @@
+const profile = {
+    username: "@rimjhimsharma",
+    isFollow: true,
+    followers: 38,
+    following: 37,
+    posts: 15,
+
+};
+console.log(profile.username);
+console.log(profile.isFollow);
+console.log(profile.followers);
+console.log(profile.following);
+console.log(profile.posts);
+//type of operator
+console.log(typeof profile["username"]);
+console.log(typeof profile["isFollow"]);
+console.log(typeof profile.followers);
+console.log(typeof profile.following);
+console.log(typeof profile.posts);
