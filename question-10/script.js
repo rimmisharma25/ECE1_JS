5b859729ad24882978e5d0b846599ac8bc965337
Rimjhim Sharma <rimjhimsharma532@gmail.com>
2026-03-07 21:48:28 +0200

    ques-10.html

---
+++ question-10/script.js
@@ -0,0 +1,8 @@
+let obj = {
+    item: "Laptop",
+    price: 50000,
+};
+console.log("The cost of " , obj.item, "is", obj.price, "rupees.");
+//Template Literals
+let specialstring = `The is a template literal`;
+console.log(typeof specialstring);
