4c5297fe4e76acb6fc1244128d51b53ee8f8e179
Rimjhim Sharma <rimjhimsharma532@gmail.com>
2026-03-07 23:41:35 +0200

    ques-18.html

--- Practice-4/script.js
+++ Practice-4/script.js
@@ -1,6 +1,6 @@
 let gameNum = 25;
  userNum = prompt("Guess the game number : ");
 while(userNum != gameNum){
-    
+    userNum = prompt("You entered the wrong number. Guess again: ");
 }
 console.log("Congratulations! You guessed the correct number: ", gameNum);
---
+++ Practice-5/script.js
@@ -0,0 +1,7 @@
+//calculate the sum of 1 to n 
+let sum = 0;
+let n = 5;
+for(let i = 1; i<=n; i++){
+    sum = sum + i;
+}
+console.log("The sum of 1 to ", n, "is =", sum);
