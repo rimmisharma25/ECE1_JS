23a3a326bdb772a371ca03be9e1167ce4dee7d2d
Rimjhim Sharma <rimjhimsharma532@gmail.com>
2026-03-07 20:11:04 +0200

    ques-3.html

---
+++ question-3/script.js
@@ -0,0 +1,21 @@
+//Unary Operators
+let a = 5;
+let b = 2;
+console.log("a = ", a, "& b = ",b);
+a++;
+console.log("a = ",a);//6
+b++;
+console.log("b = ",b);//3
+// a++ and b++ are post-increment operators, which means that the value of a and b will be incremented after the current expression is evaluated. So, when we log the value of a and b after the increment, we see the updated values.
+a--;
+console.log("a = ",a);//5
+b--;
+console.log("b = ",b);//2
+// take new values
+let x= 5;
+let y= 2;
+console.log("x = ", x, "& y = ",y);
+x--;
+console.log("x = ",x);//4
+y--;
+console.log("y = ",y);//1
