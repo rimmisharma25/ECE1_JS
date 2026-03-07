7d6eb6488ad9adac09ddae4900413549e9ee8df4
Rimjhim Sharma <rimjhimsharma532@gmail.com>
2026-03-07 19:18:48 +0200

    ques.html

---
+++ question-1/script.js
@@ -0,0 +1,12 @@
+//use of const
+const student = {
+    fullName: "Rahul Sharma",
+    age: 20,
+    grade: "A",
+    isPAss: true,
+}
+student["age"]= student["age"] + 1;
+console.log(student["age"]);
+console.log(student.fullName);
+console.log(student.grade);
+console.log(student.isPAss);
