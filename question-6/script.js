d8773ccfac6e576328f6ea66961ac19b57e33398
Rimjhim Sharma <rimjhimsharma532@gmail.com>
2026-03-07 21:14:12 +0200

    ques-6.html

---
+++ question-6/script.js
@@ -0,0 +1,9 @@
+//Logical Operators
+let a = 6;
+let b = 3;
+let cond1 = a>b;//true
+let cond2 = a<b;//false
+console.log("cond1 && cond2: ", cond1 && cond2);//false
+console.log("cond1 || cond2: ", cond1 || cond2);//true
+console.log("!cond1: ", !cond1);//false
+console.log("!cond2: ", !cond2);//true
