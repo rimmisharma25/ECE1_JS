efc7fd9705380498f97989aaca0ac42e77f4a326
Rimjhim Sharma <rimjhimsharma532@gmail.com>
2026-03-07 22:05:20 +0200

    ques-11.html

---
+++ question-11/script.js
@@ -0,0 +1,13 @@
+//String
+//Concat two string
+let str1 = "Rimjhim";
+let str2 = "Sharma";
+let result =" I am " + str1 +" " + str2;// str1 + str2, str1.concat(str2) also works
+console.log(result);
+//replace a substring with another string
+let str = "Hello";
+console.log(str.replace("lo","p"));//Help
+
+//str.slice
+let str4= "038599";
+console.log(str4.slice(1,4));//385
