0a3adf66d5570ab42b33ba2642e32df1d21677f4
Rimjhim Sharma <rimjhimsharma532@gmail.com>
2026-03-10 17:45:58 +0530

    ques-22.html

---
+++ question-22/script.js
@@ -0,0 +1,13 @@
+// funtion 
+var sayHello = function() {
+    alert('Hello World');
+}
+//array
+var numberArray = [1,2,3,4,5];
+var animals = new Array("Dog", "Cat", "Elephant");
+//object
+var person = {
+    name: "John",
+    age: 30,
+    city: "New York"
+};
