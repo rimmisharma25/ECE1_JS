6cc69dae53fe9bebe4d5232beb777452d3005ff5
Rimjhim Sharma <rimjhimsharma532@gmail.com>
2026-03-11 09:16:57 +0530

    ques-25.html

---
+++ question-25/script.js
@@ -0,0 +1,11 @@
+//for(key in object){
+    //code block to be executed
+
+const person = {
+    fname: "Rimjhim",
+    lname: "Sharma",
+    age: 20
+};
+for(let x in person){
+    console.log("person details: " + x + ":"+ person[x]);
+}
