aad405bbc31ce6893e2272b6e55d2170377b225c
Rimjhim Sharma <rimjhimsharma532@gmail.com>
2026-04-06 15:29:32 +0530

    index7.html

---
+++ Functions/practice-7/script.js
@@ -0,0 +1,16 @@
+// sum function
+function sum(a,b){
+    return a+b;
+}
+//arrowsum
+const arrowSum  = (a,b) => {
+    console.log(a+b);
+};
+//multiplication function
+function mul(a,b){
+    return a*b;
+}
+//arrowMul
+const arrowMul = (a,b) => {
+    console.log(a*b);
+};
