8c5576cec4c3f1443a31acbde7fe6b29830ca54c
Rimjhim Sharma <rimjhimsharma532@gmail.com>
2026-04-06 15:58:23 +0530

    index.html

---
+++ DOM/documents/script.js
@@ -0,0 +1,8 @@
+// let heading = document.getElementById("heading");//h1
+// console.dir(heading);
+let heading = document.getElementsByClassName("heading2");//h1
+console.log(heading);
+console.dir(heading);
+//para
+let para = document.getElementsByTagName("p");
+console.dir(para);
