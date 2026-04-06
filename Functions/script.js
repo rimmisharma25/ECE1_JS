33df17877e7b5451a4144190ffc009b1ea620b1c
Rimjhim Sharma <rimjhimsharma532@gmail.com>
2026-04-06 13:41:26 +0530

    ques-1.html

---
+++ Functions/script.js
@@ -0,0 +1,6 @@
+let arr = [1,2,3,4,5];
+let evenArr = arr.filter((val) =>{
+
+ return val%2 === 0;
+});
+console;evenArr.toLocaleString(evenArr);
---
+++ questions/script.js
@@ -0,0 +1,6 @@
+let marks = [97,64,35,49,99,97,64];
+let toppers = marks.filter((val) => {
+    return val> 90;
+
+})
+console.log(toppers);
