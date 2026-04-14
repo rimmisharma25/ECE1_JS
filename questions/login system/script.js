20072957569000c20f873c2476d0a3f45e1324b4
Rimjhim Sharma <rimjhimsharma532@gmail.com>
2026-04-14 09:33:03 +0530

    login.html

---
+++ questions/login system/script.js
@@ -0,0 +1,7 @@
+let username = prompt("Enter your username:");
+let password = prompt("Enter your password:");
+if (username === "Rimjhim Sharma" && password === "25062007") {
+    alert("Login successful!");
+} else {
+    alert("Invalid username or password. Please try again.");
+}
